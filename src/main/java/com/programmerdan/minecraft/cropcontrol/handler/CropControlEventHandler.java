b75a9968a1146fbd5879b7e3db30e0f020e5e26f
ProgrammerDan <programmerdan@gmail.com>
2017-02-15 20:50:52 -0500

    rough framing for crop control

---
+++ src/main/java/com/programmerdan/minecraft/cropcontrol/handler/CropControlEventHandler.java
@@ -0,0 +1,45 @@
+package com.programmerdan.minecraft.cropcontrol.handler;
+
+/**
+ * Simple monitor for all growth and break events and such.
+ * 
+ * @author <a href="mailto:programmerdan@gmail.com">ProgrammerDan</a>
+ *
+ */
+public class CropControlEventHandler {
+	// Track growable placement, regardless of if configured to or not.
+	//   Crops, as of 1.12: (event.block.BlockGrowEvent)
+	//     * WHEAT (Use Crops)
+	//     * CARROT (Use Crops)
+	//     * POTATO (Use Crops)
+	//     * NETHERWART (Use Crops)
+	//     * BEETROOT (Use Crops)
+	//     * COCOA (Use CocoaPlant)
+	//     * PUMPKIN
+	//     * MELON
+	//     * CACTUS
+	//   Spreadables: (event.block.BlockSpreadEvent)
+	//     * RED MUSHROOM
+	//     * BROWN MUSHROOM
+	//     * 
+	//   Growables: (event.world.StructureGrowEvent)
+	//     * Acacia
+	//     * Oak (Big)
+	//     * Birch
+	//     * Brown Mushroom
+	//     * Chorus Plant
+	//     * Cocoa Tree (Jungle w/ cocoa)
+	//     * Dark Oak (2x2)
+	//     * Jungle (2x2 tall)
+	//     * Jungle (bush)
+	//     * Mega Spruce / redwood (2x2 tall)
+	//     * Red Mushroom
+	//     * Spruce / redwood (1x1)
+	//	   * Jungle (small)
+	//     * Swamp (w/ vines)
+	//     * Birch (tall)
+	//     * Spruce / redwood (tall)
+	//     * Oak (normal)
+	//
+	
+}
